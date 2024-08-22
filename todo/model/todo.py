9abf90d923264f7e7a929ca351791ca46f77bcb5
Felipe Ortiz <fortizalvaran@gmail.com>
2024-08-22 12:23:51 -0500

    clase todo y metodos mark completed add tag y metodo especial str

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,25 @@
 # TODO: Add code here
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = False
+        self.tags = []
+        
+        def mark_completed(self):
+            self.completed = True
+            
+        def add_tag(self, tag: str):
+            if tag not in self.tags:
+                self.tags.append(tag)
+                
+        def __str__(self) -> str:
+            return f"{self.code_id} - {self.title}"
+        
+class TodoBook:
+    def __init__(self):
+        self.todos = {}                
+                
+                
+        
